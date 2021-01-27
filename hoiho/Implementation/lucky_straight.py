39ff63ff7d17172c5da38a49f79d56e7c0bb3bfb
jiyoung yoon <65146746+hoiho77@users.noreply.github.com>
2021-01-28 02:03:49 +0900

    Update lucky_straight.py

--- hoiho/Implementation/lucky_straight.py
+++ hoiho/Implementation/lucky_straight.py
@@ -1,1 +1,12 @@
+#p.321 Q7
 
+#@time_check
+def solution(n):
+    split_p = int(len(n)/2)
+    sum_1 = sum(list(map(int, [i for i in n[:split_p]])))
+    sum_2 = sum(list(map(int, [j for j in n[split_p:]])))
+    return "LUCKY" if sum_1 == sum_2 else "READY"
+
+if __name__ == '__main__':
+    n = input()
+    print(solution(n))
